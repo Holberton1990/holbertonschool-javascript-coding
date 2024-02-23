66437464f90ac9f1bd3725b1c77cc3c3940e0e30
Holberton1990 <135588172+Holberton1990@users.noreply.github.com>
2024-02-23 01:09:58 +0100

    Update 2-statuscode.js

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
 const request = require('request');
-const process = require('process');
 
-const link = process.argv[2];
-
-request
-  .get(link)
-  .on('response', function (response) {
-    console.log('code: ' + response.statusCode);
-  });
+request(process.argv[2], (error, response, body) => {
+  if (error) {
+    console.log(error);
+  }
+  console.log(`code: ${response && response.statusCode}`);
+});
