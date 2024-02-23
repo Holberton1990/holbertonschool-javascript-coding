dbd5039f261770acb6065452fda5e62130d9de27
Holberton1990 <135588172+Holberton1990@users.noreply.github.com>
2024-02-23 01:09:30 +0100

    Update 1-writeme.js

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,10 +1,7 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-const arg2 = process.argv[2];
-const arg3 = process.argv[3];
 
-fs.writeFile(arg2, arg3, err => {
+fs.writeFile(process.argv[2], process.argv[3], { encodig: 'utf-8' }, (err) => {
   if (err) {
     console.log(err);
   }
