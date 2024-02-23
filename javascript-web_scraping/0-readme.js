13d3b68055941d6f40afca4e834e02fc7a251ae5
Holberton1990 <135588172+Holberton1990@users.noreply.github.com>
2024-02-23 01:08:49 +0100

    Update 0-readme.js

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,6 +1,10 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', (err, data) => {
-  if (err) throw err;
+
+fs.readFile(process.argv[2], 'utf-8', (err, data) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
   console.log(data);
 });
