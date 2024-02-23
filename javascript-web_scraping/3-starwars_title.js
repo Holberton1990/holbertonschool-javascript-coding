bd3758babe26c2c7bbd9662e156f3bf24a9f6b28
Holberton1990 <135588172+Holberton1990@users.noreply.github.com>
2024-02-23 01:10:37 +0100

    Update 3-starwars_title.js

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,16 +1,14 @@
 #!/usr/bin/node
+
 const request = require('request');
-const process = require('process');
 
-const link = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(
+  `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`,
+  (error, response, body) => {
+    if (error) {
+      console.log(error);
+    }
 
-request(link, (error, response, body) => {
-  if (error) {
-    console.error(error);
-  } else if (response.statusCode !== 200) {
-    console.error(`Response code: ${response.statusCode}`);
-  } else {
-    const results = JSON.parse(body);
-    console.log(results.title);
+    console.log(JSON.parse(body).title);
   }
-});
+);
